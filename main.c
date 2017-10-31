72182fec2fef7aefac12f329f9d45c7cbd31ac01
Kyle Lin <klin12@stuy.edu>
2017-10-31 16:12:26 -0400

    Added human readable file size

--- main.c
+++ main.c
@@ -7,4 +7,14 @@ int main() {
     printf("The permissions of main.c are: %o\n", sb.st_mode);
     char *timeaccess = ctime(&sb.st_atime);
     printf("The time of last access of main.c is: %s", timeaccess);
+
+    float size = (float)sb.st_size;
+    char *units[] = {"B", "kb", "mb", "gb"};
+    int counter = 0;
+    while (size > 1024) {
+        size /= 1024;
+        counter++;
+    }
+
+    printf("Size of make.c: %f %s\n", size, units[counter]); 
 }
